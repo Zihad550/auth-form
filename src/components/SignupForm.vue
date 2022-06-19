36d12d30638dcd8a6b6ea85dbaaa653e26bbc043
Zihad550 <hussainmuzahid83@gmail.com>
2022-06-19 17:00:05 +0600

    feat: initial setup completed

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,70 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <label>Email:</label>
+    <input type="email" required v-model="email" />
+
+    <label>Password:</label>
+    <input type="password" required v-model="password" />
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+    <button type="submit">Sign up</button>
+  </form>
+
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+</template>
+
+<script>
+export default {
+  name: "SignupForm",
+  data() {
+    return {
+      email: "",
+      password: "",
+      role: "developer",
+    };
+  },
+
+  methods: {
+    handleSubmit() {
+      console.log(this.email, this.password);
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+input,
+select {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+  margin-bottom: 5px;
+}
+</style>
